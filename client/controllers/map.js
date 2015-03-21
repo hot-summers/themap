48ef99645c06055420a2b3c01ee5860983191f52
Kevin Green <thedudem4n@gmail.com>
2015-03-20 22:04:39 -0400

    i smart like wizard

--- client/controllers/map.js
+++ client/controllers/map.js
@@ -1,15 +1,21 @@
 Template.map.rendered = function(){
-  $('svg').css({width: 800, height: 400});
+  $('svg').css({width: 800, height: 800});
   $(window).resize(function() {
     console.log('cool');
   });
 };
 
 Template.navigation.events({
-  'click li': function(e) {
+  'mouseover li': function(e) {
+    console.log(e);
     e.preventDefault();
     var $highlight = $(e.currentTarget).attr('class');
+    console.log($highlight);
 
-    $('svg .'+$highlight).addClass('active');
+    $('svg path').attr('class', 'hidden');
+    $("svg path[data-legality='legal']").attr('class', 'active');
+  },
+  'mouseout li': function(e) {
+    $('svg path').attr('class', '');
   }
 })
